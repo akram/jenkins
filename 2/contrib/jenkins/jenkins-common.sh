078e545749e87a18e2566ad6d7cc6be890d2472b
Akram Ben Aissi <akram.benaissi@gmail.com>
2020-08-12 14:46:41 +0200

    Adds full path for libnss_wrapper

--- 2/contrib/jenkins/jenkins-common.sh
+++ 2/contrib/jenkins/jenkins-common.sh
@@ -15,7 +15,7 @@ function generate_passwd_file() {
   export GROUP_ID=$(id -g)
   grep -v -e ^default -e ^$USER_ID /etc/passwd > "$HOME/passwd"
   echo "default:x:${USER_ID}:${GROUP_ID}:Default Application User:${HOME}:/sbin/nologin" >> "$HOME/passwd"
-  export LD_PRELOAD=libnss_wrapper.so
+  export LD_PRELOAD=/usr/lib64/libnss_wrapper.so
   export NSS_WRAPPER_PASSWD=${HOME}/passwd
   export NSS_WRAPPER_GROUP=/etc/group
 }
